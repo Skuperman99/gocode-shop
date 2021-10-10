b089f4ea0e75b346e798d585d9ae9425a2d86bd4
שירה קופרמן <shira9080740@gmail.com>
2021-10-10 17:16:23 +0300

    first time

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,4 +1,7 @@
-const Product = () => (
+import './Product.css';
+
+function Product() {
+  return (
     <div className="product-card">
       <div className="product-image">
         <img src="https://cdn.shopify.com/s/files/1/0938/8938/products/10231100205_1_1315x1800_300_CMYK_1024x1024.jpeg?v=1445623369" />
@@ -9,3 +12,5 @@ const Product = () => (
       </div>
     </div>
   );
+}
+  export default Product;
