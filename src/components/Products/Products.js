41146d24abc2301f8cea02bb3f607a5a0c67d9a9
שירה קופרמן <shira9080740@gmail.com>
2021-10-12 18:03:24 +0300

    second run

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,16 +3,21 @@ import Product from '../Product/Product';
 import './Products.css';
 
 
-function Products() {
+function Products({productsL}) {
   return (
-    <section className="products">
-        <Product/>
-        <Product/> 
-        <Product/> 
-        <Product/> 
-        <Product/> 
-        <Product/> 
+    <section className="Products">
+     {productsL.map((product) => (
+   < Product title={product.title}
+              key={product.id} 
+              price={product.price} 
+              description={product.description}
+              category={product.category}
+              image={product.image}
+              rating={product.rating}
+              rate={product.rate}
+              count={product.count}/>
+     ))}
     </section>
   );
-}
+     }
   export default Products;
